5e288fd08ee9966ae4f156d8dfebb70aa3aa7e75
172161401@qq.com <onlyloveyou0725>
2020-05-12 11:11:51 +0800

    WindowWordCount Demo

--- flink-quickstart/src/main/java/com/jie/flink/SocketTextStreamWordCount.java
+++ flink-quickstart/src/main/java/com/jie/flink/SocketTextStreamWordCount.java
@@ -5,55 +5,35 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 
 public class SocketTextStreamWordCount {
     public static void main(String[] args) throws Exception {
-        //参数检查
-        if (args.length != 2) {
-            System.err.println("USAGE:\nSocketTextStreamWordCount <hostname> <port>");
-            return;
-        }
-
-        String hostname = args[0];
-        Integer port = Integer.parseInt(args[1]);
-
 
         // set up the streaming execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-//        env.addSource(null);
-//        env.fromCollection(null); // 从 Java 的 Java.util.Collection 创建数据流。集合中的所有元素类型必须相同
-//        env.fromCollection(null, Integer.class); // 从一个迭代器中创建数据流。Class 指定了该迭代器返回元素的类型。
-//        env.fromElements(
-//                new Event(1, "barfoo", 1.0),
-//                new Event(2, "start", 2.0),
-//                new Event(3, "foobar", 3.0)
-//        );
-
 
         //获取数据
-        DataStreamSource<String> stream = env.socketTextStream(hostname, port);
+        DataStreamSource<String> stream = env.socketTextStream("localhost", 9000);
 
         //计数
-        SingleOutputStreamOperator<Tuple2<String, Integer>> sum = stream.flatMap(new LineSplitter())
+        SingleOutputStreamOperator<Tuple2<String, Integer>> dataStream  = stream.flatMap(new LineSplitter())
                 .keyBy(0)
+                .timeWindow(Time.seconds(15))
                 .sum(1);
 
-        sum.print();
+        dataStream.print();
 
-        env.execute("Java WordCount from SocketTextStream Example");
+        env.execute("Window WordCount");
     }
 
     public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
         @Override
-        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) {
-            String[] tokens = s.toLowerCase().split("\\W+");
-
-            for (String token: tokens) {
-                if (token.length() > 0) {
-                    collector.collect(new Tuple2<String, Integer>(token, 1));
-                }
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> out) {
+            for (String word : s.split(" ")) {
+                out.collect(new Tuple2<>(word, 1));
             }
         }
     }
