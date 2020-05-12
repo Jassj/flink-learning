16b091e2c46353317fb848e68391fb4b42a3da14
172161401@qq.com <onlyloveyou0725>
2020-05-12 12:14:17 +0800

    A variety of Data Sources

--- flink-quickstart/src/main/java/com/jie/flink/DataSourceStream.java
+++ flink-quickstart/src/main/java/com/jie/flink/DataSourceStream.java
@@ -1,7 +1,6 @@
 package com.jie.flink;
 
 import com.google.common.collect.Lists;
-import com.jie.flink.sources.SourceFromMySQL;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
@@ -10,26 +9,24 @@ import java.util.ArrayList;
 public class DataSourceStream {
 
     public static void main(String[] args) throws Exception {
+
+        // create execution environment: 根据上下文环境创建
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // Data Source from Collection.class: 有界数据集, 偏向本地测试
         ArrayList<String> list = Lists.newArrayList(
                 "hello", "my", "name", "is", "leo"
         );
+
         env.fromCollection(list).print();
-//        env.fromCollection(list.iterator(), String.class).print();
 
         // Data Source from File: 适合监听文件修改并读取其内容
         env.readTextFile("D:\\CommonUnits\\flink-1.10.0\\conf\\flink-conf.yaml").print();
 
-        // Data Source from socket
-//        env.socketTextStream("localhost", 9000) // 监听 localhost 的 9000 端口过来的数据
-//                .flatMap(new Splitter())
-//                .keyBy(0)
-//                .timeWindow(Time.seconds(5))
-//                .sum(1);
+        // Data Source from socket: 监听 localhost 的 9000 端口过来的数据
+        env.socketTextStream("localhost", 9000);
 
-        // customized source: 继承并实现SourceFunction接口
+        // customized source: 继实现SourceFunction接口
         env.addSource(new SourceFunction<Object>() {
 
             private static final long serialVersionUID = 1L;
@@ -37,7 +34,7 @@ public class DataSourceStream {
             private volatile boolean isRunning = true;
 
             @Override
-            public void run(SourceContext<Object> sourceContext) throws Exception {
+            public void run(SourceContext<Object> sourceContext) {
                 while(isRunning) {
                     sourceContext.collect(Math.floor(Math.random() * 100));
                 }
@@ -49,10 +46,6 @@ public class DataSourceStream {
             }
 
         }).print();
-
-        // Data Source from MySQL
-        env.addSource(new SourceFromMySQL()).print();
-
 
         // execute job
         env.execute("Flink add data source");
