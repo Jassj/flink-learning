15de5b0011448d0fbcc2b5e00a596cffd33bba93
袁杰 <onlyloveyou0725>
2020-09-01 13:50:35 +0800

    feat: update basic functions & add window functions

--- flink-quickstart/src/main/java/com/jie/flink/modules/service/DataSourceTransform.java
+++ flink-quickstart/src/main/java/com/jie/flink/modules/service/DataSourceTransform.java
@@ -12,81 +12,65 @@ import org.apache.flink.streaming.api.windowing.time.Time;
 
 public class DataSourceTransform {
 
-    protected static void initEnv(StreamExecutionEnvironment env) {
-        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-    }
+	// basic operators to transform number
+	public static void transformNumber(DataStreamSource<String> dataStreamSource) {
+		dataStreamSource.flatMap((FlatMapFunction<String, String>) (s, collector) -> {
+			for (String word : s.split(" ")) {
+				collector.collect(word);
+			}
+		}).returns(Types.STRING).map((MapFunction<String, Integer>) s -> Integer.parseInt(s) * 9).filter(value -> value % 2 == 0).print();
+	}
 
-    // basic operators to transform number
-    public static void transformNumber(DataStreamSource<String> dataStreamSource) {
-        dataStreamSource.flatMap((FlatMapFunction<String, String>) (s, collector) -> { // flatMap: 按空格分割字符串
-            for(String word : s.split(" ")) {
-                collector.collect(word);
-            }
-        }).returns(Types.STRING
-        ).map((MapFunction<String, Integer>) s -> Integer.parseInt(s) * 9             // map: 字符串转数字并 * 9
-        ).filter((FilterFunction<Integer>) value -> value % 2 == 0                    // filter: 过滤所有奇数
-        ).print();
-    }
+	// transform dataStream to Object
+	public static void transformObject(DataStreamSource<String> dataStreamSource) {
+		dataStreamSource.flatMap((FlatMapFunction<String, Student>) (s, collector) -> {
+			for (String student : s.split(" ")) {
+				int id = Integer.parseInt(student.split(":")[0]);
+				String name = student.split(":")[1];
+				collector.collect(new Student(id, name, name, (int) Math.round(Math.random() * 100 + 1)));
+			}
+		}).returns(Student.class).filter(student -> student.getId() % 2 == 0).keyBy(Student::getId)
+		        .reduce((student, t1) -> new Student(student.getId(), student.getName() + "-" + t1.getName(),
+		                student.getPassword() + "-" + t1.getPassword(), (student.getAge() + t1.getAge()) / 2))
+		        .print();
+	}
 
-    // transform dataStream to Object
-    public static void transformObject(DataStreamSource<String> dataStreamSource) {
-        dataStreamSource.flatMap((FlatMapFunction<String, Student>) (s, collector) -> {
-            for(String student : s.split(" ")) {
-                int id = Integer.parseInt(student.split(":")[0]);
-                String name = student.split(":")[1];
-                collector.collect(new Student(id, name, name, (int) Math.round(Math.random() * 100 +1)));
-            }
-        }).returns(Student.class)
-                .filter((FilterFunction<Student>) student -> student.getId() % 2 == 0)
-                .keyBy(Student::getId)
-                .reduce((ReduceFunction<Student>) (student, t1) -> { // 归并操作: 求相同ID的学生年龄平均值
-                    return new Student(student.getId(), student.getName() + "-" + t1.getName(),
-                            student.getPassword() + "-" + t1.getPassword(), (student.getAge() + t1.getAge()) / 2);
-                }).print();
-    }
+	public static void aggregationOperators(DataStreamSource<String> dataStreamSource) {
+		dataStreamSource.flatMap((FlatMapFunction<String, Tuple3<String, Integer, Integer>>) (s, collector) -> {
+			for (String subStr : s.split(" ")) {
+				int i = (int) Math.floor(Math.random() * 100 + 1);
+				collector.collect(Tuple3.of(subStr, i, i));
+			}
+		}).returns(Types.TUPLE(Types.STRING, Types.INT, Types.INT)).keyBy(0).maxBy(2).print();
+	}
 
-    public static void aggregationOperators(DataStreamSource<String> dataStreamSource) {
-        dataStreamSource.flatMap((FlatMapFunction<String, Tuple3>) (s, collector) -> {
-            for(String subStr : s.split(" ")) {
-                int i = (int) Math.floor(Math.random() * 100 + 1);
-                collector.collect(Tuple3.of(subStr, String.valueOf(i), i));
-            }
-        }).returns(Types.TUPLE(Types.STRING, Types.STRING, Types.INT)
-        ).keyBy(0
-//        ).max(2
-        ).maxBy(2
-        ).print();
-    }
+	// window operators: time window & count window
+	public static void windowOperators(DataStreamSource<String> dataStreamSource) {
+		dataStreamSource.map(s -> Tuple2.of(s, (int) Math.round(Math.random() * 100 + 1))).returns(Types.TUPLE(Types.STRING, Types.INT))
+		        .keyBy(0)
+				.timeWindow(Time.seconds(10))
+		        .allowedLateness(Time.seconds(1))
+		        .reduce((t1, t2) -> Tuple2.of(t1.f0, t1.f1 + t2.f1))
+				.print();
+	}
 
-    // window operators: time window & count window
-    public static void windowOperators(DataStreamSource<String> dataStreamSource) {
-        dataStreamSource.map((MapFunction<String, Tuple2<String, Integer>>) s -> Tuple2.of(s, (int) Math.round(Math.random() * 100 +1))
-        ).returns(Types.TUPLE(Types.STRING, Types.INT)).keyBy(0
-        ).timeWindow(Time.seconds(10)                     // 滚动(不重合)窗口: tumbling time window 每10s统计一次
-//        ).timeWindow(Time.seconds(30), Time.seconds(10) // 滑动窗口: sliding time window 每10s统计过去30s的数据
-//        ).countWindow(10       // 统计10的元素
-//        ).countWindow(100, 10  // 每10的元素统计100的元素
-        ).reduce((ReduceFunction<Tuple2<String, Integer>>) (t1, t2) -> Tuple2.of(t1.f0, t1.f1 + t2.f1) // 等价于sum(1)
-        ).print();
-    }
+	public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws Exception {
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        initEnv(env);
+		//		获取数据: 监听本地9000端口 nc -l -p 9000
+		DataStreamSource<String> dataStreamSource = env.socketTextStream("localhost", 9000);
 
-        //获取数据: 监听本地9000端口
-        DataStreamSource<String> dataStreamSource = env.socketTextStream("localhost", 9000);
+		//		transformNumber(dataStreamSource);
 
-//        transformNumber(dataStreamSource);
+		//		transformObject(dataStreamSource);
 
-//        transformObject(dataStreamSource);
+		//		aggregationOperators(dataStreamSource);
 
-//        aggregationOperators(dataStreamSource);
+		windowOperators(dataStreamSource);
 
-        windowOperators(dataStreamSource);
+		env.execute("DataStream Transform Test");
 
-        env.execute("DataStream Transform Test");
-    }
+	}
 
 }
---
+++ flink-quickstart/src/main/java/com/jie/flink/modules/service/WindowFunctionDemo.java
@@ -0,0 +1,102 @@
+package com.jie.flink.modules.service;
+
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class WindowFunctionDemo {
+
+	/**
+	 * processWindowFunction
+	 * @param dataStreamSource 数据源
+	 */
+	private static void processFunction(DataStreamSource<String> dataStreamSource) {
+		dataStreamSource.map(s -> Tuple2.of(s, (int) Math.round(Math.random() * 100 + 1))).returns(Types.TUPLE(Types.STRING, Types.INT))
+		        .keyBy(0)
+				.timeWindow(Time.seconds(10))
+		        .process(new ProcessWindowFunction<Tuple2<String, Integer>, String, Tuple, TimeWindow>() {
+			        @Override
+			        public void process(Tuple tuple, Context context, Iterable<Tuple2<String, Integer>> elements, Collector<String> out)
+			                throws Exception {
+				        AtomicInteger sum = new AtomicInteger();
+				        elements.forEach(element -> {
+					        sum.addAndGet(element.f1);
+				        });
+				        out.collect(tuple.getField(0) + " --> sum: " + sum.get());
+			        }
+		        }).print();
+	}
+
+	/**
+	 * processWindowFunction & ReduceFunction
+	 * @param dataStreamSource 数据源
+	 */
+	private static void processFunctionWithReduce(DataStreamSource<String> dataStreamSource) {
+		dataStreamSource.map(s -> Tuple2.of(s, (int) Math.round(Math.random() * 100 + 1))).returns(Types.TUPLE(Types.STRING, Types.INT))
+				.keyBy(0)
+				.timeWindow(Time.seconds(10))
+				.reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+					@Override
+					public Tuple2<String, Integer> reduce(Tuple2<String, Integer> t1, Tuple2<String, Integer> t2) {
+						return t1.f1 > t2.f1 ? t1 : t2;
+					}
+				}, new ProcessWindowFunction<Tuple2<String, Integer>, Object, Tuple, TimeWindow>() {
+					@Override
+					public void process(Tuple tuple, Context context, Iterable<Tuple2<String, Integer>> elements, Collector<Object> out) {
+						Tuple2<String, Integer> element = elements.iterator().next();
+						out.collect(new Tuple2<>(context.window().getStart(), element.f1));
+					}
+				})
+				.print();
+	}
+
+	/**
+	 * windowFunction
+	 * @param dataStreamSource 数据源
+	 */
+	private static void applyFunction(DataStreamSource<String> dataStreamSource) {
+		dataStreamSource.map(s -> Tuple2.of(s, (int) Math.round(Math.random() * 100 + 1))).returns(Types.TUPLE(Types.STRING, Types.INT))
+				.keyBy(0)
+				.timeWindow(Time.seconds(10))
+				.apply(new WindowFunction<Tuple2<String, Integer>, Object, Tuple, TimeWindow>() {
+					@Override
+					public void apply(Tuple tuple, TimeWindow window, Iterable<Tuple2<String, Integer>> input, Collector<Object> out) throws Exception {
+						AtomicReference<Long> sum = new AtomicReference<>(0L);
+						input.forEach(element -> {
+							sum.updateAndGet(v -> v + element.f1);
+						});
+						out.collect("sum: " + sum.get());
+					}
+				})
+				.print();
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		//	获取数据: 监听本地9000端口 nc -l -p 9000
+		DataStreamSource<String> dataStreamSource = env.socketTextStream("localhost", 9000);
+
+//		processFunction(dataStreamSource);
+
+//		processFunctionWithReduce(dataStreamSource);
+
+		applyFunction(dataStreamSource);
+
+		env.execute("DataStream Transform Test");
+
+	}
+
+}
